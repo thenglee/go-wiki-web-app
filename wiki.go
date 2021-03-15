729e2ad1b7753a6b35fb15fcb3105dc17952c929
Tan Theng Lee <thengleetan@gmail.com>
2021-03-15 15:16:57 +0800

    Add edit pages

--- wiki.go
+++ wiki.go
@@ -32,6 +32,20 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
 }
 
+func editHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/edit/"):]
+	p, err := loadPage(title)
+	if err != nil {
+		p = &Page{Title: title}
+	}
+	fmt.Fprintf(w, "<h1>Editing %s</h1>"+
+		"<form action=\"/save/%s\" method=\"POST\">"+
+		"<textarea name=\"body\">%s</textarea><br>"+
+		"<input type=\"submit\" value=\"Save\">"+
+		"</form>",
+		p.Title, p.Title, p.Body)
+}
+
 func main() {
 	p1 := &Page{Title: "TestPage", Body: []byte("This is a sample page.")}
 	p1.save()
@@ -39,5 +53,6 @@ func main() {
 	fmt.Println(string(p2.Body))
 
 	http.HandleFunc("/view/", viewHandler)
+	http.HandleFunc("/edit/", editHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
